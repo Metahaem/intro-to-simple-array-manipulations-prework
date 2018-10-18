d90df7cd1817410072218ff7cd2384eee391cf79
Tom Rowson <tom.rowson@gmail.com>
2018-10-18 15:50:42 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,4 +27,12 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.using_uniq
+end
+
+def using_delete(array, string)  
   
